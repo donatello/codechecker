49d50de9078506996cc2c9f46860c17f61f2a1d3
surenspost <none@none>
2010-02-13 22:15:58 +0000

    Basic Project Structure
    site/ - Django Root
    media/ - static css, js and images files being served
    utils/ - scripts, config files
    
    --HG--
    branch : 2010

--- site/generic_views.py
+++ site/generic_views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def default(request):
-    return HttpResponse(" <h1> About Page ! </h2> ");
+    return HttpResponse(" <h1> About Page ! </h2> <h3> More to come, soon ! </h3>");
