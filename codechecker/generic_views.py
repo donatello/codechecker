40fc3a4e280989085a4293af955c2399fac11a28
surenspost <none@none>
2010-02-15 13:25:05 +0000

    Version 0.1
    --HG--
    branch : 2010

--- codechecker/generic_views.py
+++ codechecker/generic_views.py
@@ -3,6 +3,6 @@ from django.template import RequestContext as Context
 from django.template import loader
 
 def default(request, action='base'):
-    template = loader.get_template('base.html')
+    template = loader.get_template( action +  '.html')
     context = Context(request)
     return HttpResponse(template.render(context))
