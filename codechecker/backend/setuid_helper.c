d0a52789ba1143ca10b375387cc3c60ff3564270
aditya87 <none@none>
2010-02-20 18:17:03 +0000

    More changes to setuid_helper
    --HG--
    branch : 2010

--- codechecker/backend/setuid_helper.c
+++ codechecker/backend/setuid_helper.c
@@ -9,10 +9,19 @@ int main(int argc, char* argv[]) {
   // fork argv[1]
   pid_t p = fork();
   if (!p) { 
+
+    //TODO: place submission in a chroot jail.
+    
     setuid(1002);    
     struct rlimit fork_lim ;
+    // set limit on number of forks possible.
     fork_lim.rlim_cur = fork_lim.rlim_max = 0; 
     int ret = setrlimit(RLIMIT_NPROC, &fork_lim);
+    
+    //set limit on the number of files that can be opened.
+    fork_lim.rlim_cur = fork_lim.rlim_max = 3;
+    ret = setrlimit(RLIMIT_NOFILE, &fork_lim);
+
     //		printf("ret = %d\n", ret);
     ret = execvp(argv[1], NULL);    
     //		printf("execvp ret = %d\n", ret);
