c0bacb94f1771dcfcb4d71969aabc2f818b515a4
mine.or.tea.re.pot <none@none>
2010-02-22 19:17:26 +0000

    Modifying install2db.py in reaction to the changes to the Lang field in DB
    --HG--
    branch : 2010

--- codechecker/backend/tests/install-tests2db.py
+++ codechecker/backend/tests/install-tests2db.py
@@ -4,7 +4,6 @@ sys.path.append("/opt/checker")
 from codechecker.contests.models import Submission, User, Problem
 
 def submit_to_db(files):
-  lang_dict = {'C' : 1, 'CPP' : 2}
   for fname in files:
     ext = fname.rsplit(".", 1)[1]
     sub = Submission()
@@ -12,7 +11,7 @@ def submit_to_db(files):
     sub.problem = Problem.objects.get(id = 1) 
     sub.result = "QU"
     sub.submissionTime = datetime.datetime.now() 
-    sub.submissionLang = lang_dict[ext.upper()]
+    sub.submissionLang = ext
     sub.submissionPenalty = 0
     sub.submissionPoints = 0
     sub.submissionCode = file(fname, "r").read()
