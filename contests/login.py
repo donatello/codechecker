6bf1a6d43a569d45957de4e6785c682c5e273d01
aditya@donatello <aditya@donatello>
2010-03-02 18:37:53 +0530

    Removed files in the old working directory to make the 2010 branch the new default.

--- contests/login.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-from django.contrib.auth.models import User, check_password
-from django.contrib.auth import authenticate, login
-
-from MySQLdb import *
-from md5 import *
