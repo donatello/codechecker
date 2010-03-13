e51f9e031385d6c856e6ad58ce63c617e16ef81b
surenspost <none@none>
2010-03-13 12:31:26 +0530

    models.py, the revamped version. A lot of things will break. Fixing them is the task for the week. !

--- codechecker/contests/models.py
+++ codechecker/contests/models.py
@@ -3,15 +3,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django import forms
 
-import datetime
-
-
+# Current Language types that are supported
 LANG_TYPES = (
     ('c','C'),
     ('cpp','C++'),
     #(3,'JAVA'),
 )
 
+# Submission Statuses
 RESULT_TYPES = (
     ('QU', 'QUEUED'),
     ('CMP', 'COMPILING'),
@@ -25,114 +24,82 @@ RESULT_TYPES = (
     ('FPE', 'FLOATING POINT ERROR'),
     ('KILL', 'KILLED'),    
     ('ABRT', 'ABORT SIGNALLED'),    
-    ('UNKN', ''),
-    ('WTF', ''),
+    ('UNKN', 'NON ZERO RETURN STATUS'),
+    ('WTF', 'NON ZERO RETURN STATUS'),
 )
 
+# The site is organized as contests, this is a basic contest model
+# Contest has a title,  a general description startTime and endTime.
+# Also if it will be publicly viewable by non admins if public is True.
+
 class Contest(models.Model):
-    title = models.CharField(max_length = 25)
+    title = models.CharField(max_length = 25) 
     description = models.TextField()
-    startDateTime = models.DateTimeField()
-    endDateTime = models.DateTimeField()
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    public = models.BooleanField(default = False)
 
     def __unicode__(self):
         return self.title
-    
-    
-class Team(models.Model):
-    user = models.ForeignKey(User) 
-    name = models.CharField(max_length=25)
-    teamSize = models.IntegerField()
-    primary_email = models.EmailField(max_length=75)
-    secondary_email = models.EmailField(max_length=75, blank=True)
-    tertiary_email = models.EmailField(max_length=75, blank=True)
-    primary = models.CharField(max_length=25)
-    secondary = models.CharField(max_length=25, blank=True)
-    tertiary = models.CharField(max_length=25, blank=True)
 
-    def __unicode__(self):
-        return self.name
-
-class TempReg(models.Model):
-    name = models.CharField(max_length=25, verbose_name = " Your team Name")
-    teamSize = models.IntegerField()
-    primary_email = models.EmailField(max_length=75, verbose_name="First Participant Email")
-    primary = models.CharField(max_length=25, verbose_name="First Participant Name")
-    secondary_email = models.EmailField(max_length=75, blank=True, verbose_name="Second Participant Email")
-    secondary = models.CharField(max_length=25, blank=True, verbose_name="Second participant Name")
-    tertiary_email = models.EmailField(max_length=75, blank=True, verbose_name="Third Participant email")
-    tertiary = models.CharField(max_length=25, blank=True, verbose_name="Third Participant Name")
-    code = models.CharField(max_length=100, blank=True, null=True, editable=False, default='hash123') 
-
-    def __unicode__(self):
-        return self.name + str(self.primary_email)
+# Each Contest has one or more problems. Orphan or non contest problem are added  
+# to a global contest. Each problem has a problem code, statement, constraint 
+# notes, sample input and output, time limit, memory limit, maximum score for 
+# that problem, allowed Languages for submission to that problem and a penalty 
+# for a wrong submission.
 
 class Problem(models.Model):
     contest = models.ForeignKey(Contest)
-    problemCode = models.CharField(max_length = 25)
-    problemStatement = models.TextField()
-    problemNotes = models.TextField()
-    inputData= models.TextField()
-    outputData= models.TextField()
+    pcode = models.CharField(max_length = 25)
+    statement = models.TextField()
+    notes = models.TextField()
+    sampleInput = models.TextField()
+    sampleOutput = models.TextField()
     tlimit = models.IntegerField()
     mlimit = models.IntegerField()
-    maxScore = models.IntegerField()
-    allowableLanguages = models.CommaSeparatedIntegerField(max_length=10)
-        
-    def __unicode__(self):
-        return self.problemCode
-        
+    allowedLangs = models.CommaSeparatedIntegerField(max_length=10)
+    penalty = models.IntegerField()
 
+    def __unicode__(self):
+        return self.pcode
+        
+# Each problem has several TestSet. A Test set is a set of tests a submission 
+# for that problem has to pass to get some score assigned to that TestSet. 
+# Testset belongs to a problem and has points.
+
+class TestSet(models.Model):
+    problem = models.ForeignKey(Problem)
+    points = models.IntegerField()
+    
+    def __unicode__(self):
+        return str(problem) + '-' + str(points)
+
+# Each TestSet has Testcases, which is an atomic test for a submission. It has 
+# an input and an corresponding Judge output to that.
+
+class Testcase(models.Model):
+    testSet = models.ForeignKey(TestSet)
+    input = models.TextField()
+    output = models.TextField()
+
+    def __unicode__(self):
+        return str(testSet)
+
+# Users can submit their solution to a problem. A submission has a result, time 
+# of submission, penalty for that submission, points for the submission, 
+# submitted code and any error message that were generated for the submission 
+# during the evaluation of the submission
 class Submission(models.Model):
     user = models.ForeignKey(User)
     problem = models.ForeignKey(Problem)
     result = models.CharField(max_length=4, choices=RESULT_TYPES, default="QU")
-    submissionTime = models.DateTimeField(default=datetime.datetime.now())
-    submissionLang = models.CharField(max_length=10, choices=LANG_TYPES)
-    submissionPenalty = models.IntegerField(default=0)
-    submissionPoints = models.IntegerField(default=0)
-    submissionCode = models.TextField()    
-    compile_errmsg = models.TextField(default="")
-    runtime = models.IntegerField(default=0)
-    memusage = models.IntegerField(default=0)
+    time = models.DateTimeField()
+    language = models.CharField(max_length=10, choices=LANG_TYPES)
+    penalty = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
+    code = models.TextField()    
+    errors = models.TextField()
     
     def __unicode__(self):
         return repr(self.pk)   
-
-class TestCase(models.Model):
-    problem = models.ForeignKey(Problem)
-    inputFile = models.TextField()
-    outputFile = models.TextField()
-    
-    def __unicode__(self):
-        testCase = 'problem' + repr(self.pk)
-        return testCase
-
-class TestSet(models.Model):
-    problem = models.ForeignKey(Problem)
-    testcase = models.ForeignKey(TestCase)
-
-    def __unicode__(self):
-        return "TestSet" + repr(self.pk)
-
-
-# Forms 
-class SubmissionForm(forms.Form):
-    SubmissionLang = forms.CharField(widget=forms.Select(choices=LANG_TYPES))
-    SubmissionCode = forms.CharField(widget=forms.Textarea(attrs={'rows':'30', 'cols':'80'}))
-
-class TempRegForm(forms.ModelForm):
-    class Meta :
-        model = TempReg
-
-class ChangePasswordForm(forms.Form):
-    pass1 = forms.CharField(max_length=50, widget=forms.PasswordInput, label="Enter New Password")
-    pass2 = forms.CharField(max_length=50, widget=forms.PasswordInput, label="Enter New Password Again")
-
-    def clean_pass2(self):
-        if len(self.cleaned_data['pass1']) < 6:
-            raise forms.ValidationError('Password must be at least 6 letters long')
-        if self.cleaned_data['pass1'] != self.cleaned_data['pass2']:
-            raise forms.ValidationError('Passwords do not match')
-        return self.cleaned_data['pass2']
 
