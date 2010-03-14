43d0a8b58dfa496929db9db0a8d8590eb6dee450
surenspost <none@none>
2010-03-14 12:48:47 +0530

    Added Default Functions for all URL handling

---
+++ codechecker/contests/problems.py
@@ -0,0 +1,23 @@
+from django.conf.urls.defaults import patterns
+
+urlpatterns = patterns( 'codechecker.contests.problems',
+        ( r'^$', 'default' ),
+        ( r'^all/', 'show_all_problems' ),
+        ( r'^all/(?P<page>\d+)/$', 'show_all_problems' ),
+        ( r'^(?P<problem_id>\d+)/$', 'show_problem' ),
+        ( r'^(?P<problem_id>\d+)/submit/$', 'problem_submit' ),
+        ( r'^(?P<problem_id>\d+)/(?P<action>\w+)/$', 'show_problem' ),
+        ( r'^(?P<problem_id>\d+)/(?P<action>\w+)/(?P<page>\d+)/$', 'show_problem' ),
+        )
+
+def default( request ):
+    pass
+
+def show_all_problems( request, page = 1 ):
+    pass
+
+def show_problem( request, problem_id, action = 'view', page = 1 ):
+    pass
+
+def submit( request, problem_id ):
+    pass
