961f7de2cb06f8695cf659304e67799b198b35d4
surenspost <none@none>
2010-04-20 03:29:10 +0530

    Creating boiler plate admin.py code

--- codechecker/contests/admin.py
+++ codechecker/contests/admin.py
@@ -1,1 +1,36 @@
+import models
+from django.contrib import admin
+
+# Admin interface for the Contest Model
+class ContestAdmin(admin.ModelAdmin):
+    pass
+
+#Admin interface for the Problem Model
+class ProblemAdmin(admin.ModelAdmin):
+    pass
+
+#Admin interface for the TestSet Model
+class TestSetAdmin(admin.ModelAdmin):
+    pass
+
+#Admin interface for the TestcaseAdmin Model
+class TestcaseAdmin(admin.ModelAdmin):
+    pass
+
+#Admin interface for the Submission Model
+class SubmissionAdmin(admin.ModelAdmin):
+    pass
+
+#Admin interface for the TestcaseEval Model
+class TestCaseEvalAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Contest, ContestAdmin)
+admin.site.register(models.Problem, ProblemAdmin)
+admin.site.register(models.TestSet, TestSetAdmin)
+admin.site.register(models.Testcase, TestcaseAdmin)
+admin.site.register(models.Submission, SubmissionAdmin)
+admin.site.register(models.TestcaseEval, TestCaseEvalAdmin)
+
+
 
