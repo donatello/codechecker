f1a57a801f0db0562210c3c9cac66529b9024161
aditya.mmy <none@none>
2010-04-21 19:38:13 +0530

    Trying to get the admin interface up

--- codechecker/contests/admin.py
+++ codechecker/contests/admin.py
@@ -5,19 +5,22 @@ class ContestAdmin(admin.ModelAdmin):
     search_fields = ['title']
 
 class ProblemAdmin(admin.ModelAdmin):
-    list_display = ( 'pcode', 'contest') 
+    list_display = ( 'pcode', 'contest', 'is_approximate', 'cust_eval', 'cust_minScore', 
+                     'cust_maxScore', 'statement', 'constraints', 'sampleInput', 'sampleOutput',
+                     'scoring_info', 'tlimit', 'mlimit', 'allowedLangs', 'source_limit') 
     search_fields = ['pcode', 'contest']
 
 class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ( 'pk', 'user', 'problem', 'result', 'points', ) 
+    list_display = ( 'pk', 'user', 'problem', 'result', 'score', 'time', 'language', 
+                     'score', 'code', 'errors') 
     search_fields = [ 'pk', 'user', 'problem', 'result', ] 
 
 class TestSetAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'problem', 'points')
-    search_fields = ['problem', 'points']
+    list_display = ('pk', 'problem', 'maxScore')
+    search_fields = ['problem', 'maxScore']
     
-class TestCaseAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'testSet')
+class TestcaseAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'testSet', 'input', 'output')
 
 admin.site.register(Contest, ContestAdmin)
 admin.site.register(Problem, ProblemAdmin)
