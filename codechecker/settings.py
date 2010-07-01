f14d976b2d7daafa38141aca42cc309c84fa3241
Surendran <surenspost@gmail.com>
2010-07-01 13:03:44 +0530

    This somehow was breaking manage.py executions. Spent an aweful time to figure out it has to be removed.

--- codechecker/settings.py
+++ codechecker/settings.py
@@ -77,7 +77,6 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = ( 
     'django.contrib.auth',
-    'django.contrib.admindocs.urls',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
