1b943b67fba546ddb5348f21ccd21ebe22ae1f08
krishnan.parthasarathi <none@none>
2010-10-29 01:41:56 +0530

    Removed secexec.py which assumes setuid helper based shared lib.

---
+++ cc_backend/src/__init__.py
@@ -0,0 +1,3 @@
+from os.path import dirname
+import sys
+sys.path.insert(0, dirname(__file__))
--- cc_backend/src/evaluator/eval.py
+++ cc_backend/src/evaluator/eval.py
@@ -1,2 +1,8 @@
+import se.secexec
 class Evaluate:
+    def eval_submission(submission, test_grp):
+        """Takes submission and test_grp and returns result_set"""
+        for testcase in test_grp:
+            pass
+        pass
     pass
